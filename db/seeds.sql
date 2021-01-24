aac3cbfea26a8a8d0f49444f07ad05c8a32715a6
shaneocon <shane.pat.oconnell@gmail.com>
2021-01-23 19:09:50 -0800

    added node modules and connection

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Cheeseburger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Double Double Animal Style", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Ol' 96er", FALSE);
+
+
