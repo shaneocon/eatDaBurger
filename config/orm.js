5397401e5323ab1dd1a2cca220b86ee392a2879c
shaneocon <shane.pat.oconnell@gmail.com>
2021-01-25 16:51:18 -0800

    added selectAll orm

--- config/orm.js
+++ config/orm.js
@@ -1,3 +1,18 @@
-selectAll()
+const connection = require("../config/connection.js");
+
+const orm = {
+    selectAll: (tableInput, callback) => {
+        const getAll = "SELECT * FROM ??";
+            connection.query(getAll, [tableInput], (error, result) => {
+                if (error) {
+                    console.log(error);
+                }
+                callback(result);
+            });
+    },
+
+}
+
+
 insertOne()
 updateOne()
