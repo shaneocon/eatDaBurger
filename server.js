2acfd9bbefde23250ab1d729b205439a226d817f
shaneocon <shane.pat.oconnell@gmail.com>
2021-01-26 18:07:31 -0800

    server.js changed env.port to env.PORT

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const exphndl = require("express-handlebars");
 const routes = require("./controllers/burgersController.js");
 const path = require("path");
-const PORT = process.env.port || 8080;
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 
